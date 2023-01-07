d0eea9e27ea08f81642e13bcbe960f52dbbda302
leen-neel <and24903@gmail.com>
2023-01-07 11:34:49 +0530

    test

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ import fetch from 'node-fetch';
 const route = process.env.ROUTE;
 
 async function get() {
-    const data = await fetch(`https://and249-api.cyclic.app/api/${route}`);
-    console.log(await data.json());
+    try {
+        const data = await fetch(`https://and249-api.cyclic.app/api/${route}`);
+        console.log(await data.json());
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 get();
